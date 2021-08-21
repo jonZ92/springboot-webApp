d840f9997201f6f37192e36c520d992b642a168e
jon-yy <zjon234@gmail.com>
2021-08-21 18:48:28 +0800

    first commit

---
+++ src/main/java/com/web/app/cn/bean/MyBean.java
@@ -0,0 +1,17 @@
+package com.web.app.cn.bean;
+
+import lombok.Data;
+
+/**
+ * @author jon
+ * @date 2021年08月21日 18:46
+ */
+
+
+@Data
+public class MyBean {
+    private String id;
+    private String name;
+    private Integer age;
+    private String hello;
+}
---
+++ src/test/java/com/web/app/cn/SpringbootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.app.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
