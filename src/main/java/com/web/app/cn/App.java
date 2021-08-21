6f1e7e1a3dc4b87bf932a0954321fc67e260d811
jon-yy <zjon234@gmail.com>
2021-08-21 19:57:30 +0800

    first commit

--- src/main/java/com/web/app/cn/App.java
+++ src/main/java/com/web/app/cn/App.java
@@ -1,16 +1,23 @@
 package com.web.app.cn;
 
+import com.web.app.cn.interceptor.UrlInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
 @EnableAsync
-public class App {
+@SpringBootApplication
+public class App implements WebMvcConfigurer {
 
     public static void main(String[] args) {
-        //mvn install:install-file -Dfile=D:\common-mongodb-1.0.jar -DgroupId=com.cnsugar -DartifactId=common -Dversion=1.0 -Dpackaging=jar
         SpringApplication.run(App.class, args);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UrlInterceptor()).addPathPatterns("/**").excludePathPatterns("/app/v1/**");
+
+    }
 }
---
+++ src/main/java/com/web/app/cn/interceptor/UrlInterceptor.java
@@ -0,0 +1,37 @@
+package com.web.app.cn.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author jon
+ * @date 2021年08月21日 19:25
+ */
+
+@Slf4j
+public class UrlInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("request path is :{}",request.getRequestURI());
+
+        if (request.getRequestURI().equals("/")) {
+            response.sendRedirect("app/v1/xxx");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //do Nothing
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //do Nothing
+    }
+}
